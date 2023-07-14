eead757c4b54d7e21d31bf89c0a0063a116a5602
Crowdbotics <team@crowdbotics.com>
2023-07-14 09:53:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-cake-42195",
+  url: "https://withered-cake-42195.botics.co",
+  api: "https://withered-cake-42195.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
